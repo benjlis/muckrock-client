401269f67ce82961909cfe502b441d1afef74681
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-01-12 07:52:33 -0500

    initial commit

---
+++ mrc.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import pandas as pd
+
+search_endpoint = "https://api.www.documentcloud.org/api/documents/search/"
+# query = {'q': 'covid', 'expand': 'user,organization'}
+query = {'q': 'covid'}
+response = requests.get(search_endpoint, params=query)
+
+results = response.json()["results"]
+count = response.json()["count"]
+next = response.json()["next"]
+
+results_str = json.dumps(results, indent=2)
+print(results_str)
+print(f'count: {count}')
+print(f'next: {next}')
+
+# df = pd.DataFrame.from_dict(response.json(), orient='index')
+df = pd.DataFrame(results)
+print(df.head())
+df.to_csv('muckrock.csv', index=False, header=True)
