5dc318be0d59dae1b6b1dd8c23b080d33d2dc052
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-01-28 15:59:39 -0500

    Add COVID-19 name variants to the query

--- mrc.py
+++ mrc.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 search_endpoint = "https://api.www.documentcloud.org/api/documents/search/"
 # query = {'q': 'covid', 'expand': 'user,organization'}
-query = {'q': 'covid'}
+query = {'q': '(covid coronavirus "covid-19" "covid19" "covid 19" "sars-cov-2" "2019-ncov")'}
 response = requests.get(search_endpoint, params=query)
 
 results = response.json()["results"]
@@ -16,7 +16,7 @@ print(results_str)
 print(f'count: {count}')
 print(f'next: {next}')
 
-# df = pd.DataFrame.from_dict(response.json(), orient='index')
+df = pd.DataFrame.from_dict(response.json(), orient='index')
 df = pd.DataFrame(results)
 print(df.head())
 df.to_csv('muckrock.csv', index=False, header=True)
